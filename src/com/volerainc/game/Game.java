6b3917b1f97970e3ecec1e0602786ed3c5a27066
VovanF98 <mr.vladtum@mail.ru>
2017-02-12 16:28:36 +0300

    next step

--- src/com/volerainc/game/Game.java
+++ src/com/volerainc/game/Game.java
@@ -1,36 +1,65 @@
 package com.volerainc.game;
 
+
 import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Dimension;
-
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable{
+public class Game extends Canvas implements Runnable
+{
+    public Boolean running = false;
 
-	private static final long serialVersionUID = 7398307276271784707L;
+    private static final long serialVersionUID = 1L;
 
-	public static final int WIDTH = 200;
-	public static final int HEIGHT = WIDTH / 4 * 3;
-	public static final int SCALE = 3;
-	public static final String NAME = "Game";
-	
-	private JFrame frame;
-	
-	public Game() {
-		setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
-		setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
-		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
-		
-		frame = new JFrame(NAME);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new BorderLayout());
-	}
-	
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
+    public static final int WIDTH = 160;
 
+    public static final int HEIGTH = WIDTH / 12 * 9;
+
+    public static final int SCALE = 3;
+
+    public static String NAME = "Game";
+
+    private JFrame frame;
+
+    public Game()
+    {
+        setMinimumSize(new Dimension(HEIGHT * SCALE, WIDTH * SCALE));
+        setMaximumSize(new Dimension(HEIGHT * SCALE, WIDTH * SCALE));
+        setPreferredSize(new Dimension(HEIGHT * SCALE, WIDTH * SCALE));
+
+        frame  = new JFrame(NAME);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.add(this, BorderLayout.CENTER);
+
+        frame.pack();
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public void run()
+    {
+        while(running)
+        {
+            System.out.println("Hello");
+        }
+    }
+
+    public synchronized void start()
+    {
+        running = true;
+        new Thread(this).start();
+    }
+
+    public synchronized void stop()
+    {
+        running = false;
+    }
+
+    public static void main(String[] args)
+    {
+        new Game().start();
+    }
 }
