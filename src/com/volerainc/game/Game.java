f09fa3596079f557801bf5a1b983d69ac9ee638d
VovanF98 <mr.vladtum@mail.ru>
2017-02-12 19:34:08 +0300

    second step

--- src/com/volerainc/game/Game.java
+++ src/com/volerainc/game/Game.java
@@ -3,7 +3,12 @@ package com.volerainc.game;
 
 import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable
@@ -19,7 +24,13 @@ public class Game extends Canvas implements Runnable
     public static final int SCALE = 3;
 
     public static String NAME = "Game";
+    
+    public int tickCount = 0;
+    
+    private BufferedImage image = new BufferedImage(WIDTH,HEIGTH,BufferedImage.TYPE_INT_RGB);
 
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+    
     private JFrame frame;
 
     public Game()
@@ -38,12 +49,81 @@ public class Game extends Canvas implements Runnable
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
+    
+    public void tick()
+    {
+    	tickCount++;
+    }
+    
+    public void render()
+    {
+    	BufferStrategy bs = getBufferStrategy();
+    	if(bs == null)
+    	{
+    		createBufferStrategy(3);
+    		return;
+    	}
+    	
+    	Graphics g = bs.getDrawGraphics();
+    	
+    	g.setColor(Color.BLACK);
+    	g.fillRect(0, 0,  getWidth(), getHeight());
+    	
+    	g.dispose();
+    	bs.show();
+    }
 
     public void run()
     {
+    	long lastTime = System.nanoTime();
+    	double nsPerTick = 1E9D/60D;
+    	
+    	int frames = 0;
+    	int ticks = 0;
+    	
+    	long lastTimer  = System.currentTimeMillis();
+    	double delta = 0;
+    	
         while(running)
         {
-            System.out.println("Hello");
+        	long now = System.nanoTime();
+        	delta += (now - lastTime) / nsPerTick;
+        	lastTime = now;
+        	boolean shouldRender = true;
+        	
+        	while(delta>=1)
+        	{
+        		ticks++;
+        		tick();
+        		delta--;
+        		shouldRender = true;
+        	}
+        	
+        	
+        	try 
+        	{
+				Thread.sleep(2);
+			} 
+        	catch (InterruptedException e) 
+        	{
+				e.printStackTrace();
+			}
+        	
+        	if(shouldRender)
+        	{
+        		frames++;
+        		render();
+        	}
+        	
+        	render();
+        	
+        	if(System.currentTimeMillis() - lastTimer >= 1000)
+        	{
+        		lastTimer += 1000;
+        		System.out.println(frames + ", " + ticks);
+        		frames = 0;
+        		ticks = 0;
+        	}
         }
     }
 
